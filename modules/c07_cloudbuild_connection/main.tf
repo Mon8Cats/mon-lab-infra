7f283b8aa9d70f8b34f438b837272b2f81f6c587
Stone Kim <Mon8Cats@gmail.com>
2024-11-16 21:33:17 -0600

    modify cloudbuild connection

--- modules/c07_cloudbuild_connection/main.tf
+++ modules/c07_cloudbuild_connection/main.tf
@@ -1,23 +1,13 @@
-terraform {
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-    }
-    google-beta = {
-      source = "hashicorp/google-beta"
-    }
-  }
-}
-
-
 resource "google_cloudbuildv2_connection" "github_connection" {
-  provider = google-beta
-  name     = var.connection_name
-  location   = var.region
+  location = var.region
+  name = var.connection_name
 
-  github {
-
+  github_config {
     app_installation_id = var.github_app_installation_id
-    personal_access_token_secret_version = "projects/${var.project_id}/secrets/${var.secret_id}/versions/latest"
+
+    authorizer_credential {
+
+      oauth_token_secret_version = "projects/${var.project_id}/secrets/${var.secret_id}/versions/latest"
+    }
   }
 }
